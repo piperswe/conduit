797f37d7a95c6be029316d87c348bbd59728bd89
Piper McCorkle <piper@conduitim.pl>
2019-10-25 20:06:49 -0700

    Make a few changes

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'gatsby';
+import classNames from 'classnames';
 
 import * as styles from './Page.module.scss';
 import Logo from './Logo';
@@ -13,7 +14,7 @@ export interface PageProps {
 
 export default function Page({ title, canonicalUrl, children }: PageProps) {
   return (
-    <div className={styles.container}>
+    <div className={classNames('container', styles.page)}>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{title ? title + ' - ' : ''}Conduit Implementations</title>
@@ -21,13 +22,15 @@ export default function Page({ title, canonicalUrl, children }: PageProps) {
           <link rel="canonical" href={'https://conduitim.pl' + canonicalUrl} />
         )}
       </Helmet>
-
-      <div className={styles.body}>
-        <Link to="/">
-          <Logo />
-        </Link>
-        {children}
-      </div>
+      <Link to="/">
+        <Logo />
+      </Link>
+      {children}
+      <footer className={styles.addrBlock}>
+        <a href="mailto:contact@conduitim.pl">contact@conduitim.pl</a> |{' '}
+        <address>380 Hamilton Ave #252, Palo Alto, CA 94301</address> |{' '}
+        <a href="tel:1-650-308-4367">+1 (650) 308-4367</a>
+      </footer>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import Img from 'gatsby-image';
 
 import * as styles from './index.module.scss';
+import * as pageStyles from '../components/Page.module.scss';
 import Logo from '../components/Logo';
 
 export const query = graphql`
@@ -27,27 +28,48 @@ export const query = graphql`
 
 export default function IndexPage({ data }) {
   return (
-    <div className={styles.container}>
+    <div className={classNames('container', pageStyles.page, 'h-card')}>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Conduit Implementations</title>
         <link rel="canonical" href="https://conduitim.pl/" />
       </Helmet>
-
-      <div className={classNames(styles.body, 'h-card')}>
-        <a className={classNames('u-url', 'u-uid')} href="https://conduitim.pl">
-          <Logo />
-        </a>
-        <Img fluid={data.file.childImageSharp.fluid} className={styles.cover} />
-        <p>
-          <a className="u-email" href="mailto:contact@conduitim.pl">
-            contact@conduitim.pl
-          </a>
-        </p>
-        <p>
-          <Link to="/services">Services</Link>
-        </p>
+      <a className={classNames('u-url', 'u-uid')} href="https://conduitim.pl">
+        <Logo />
+      </a>
+      <Img fluid={data.file.childImageSharp.fluid} className={styles.cover} />
+      <div className="row">
+        <div className="col-3"></div>
+        <Link
+          to="/services"
+          className={classNames(
+            'btn',
+            'btn-outline-primary',
+            'btn-block',
+            'col-6',
+            styles.servicesButton,
+          )}
+        >
+          Services
+        </Link>
+        <div className="col-3"></div>
       </div>
+      <footer className={pageStyles.addrBlock}>
+        <a className="u-email" href="mailto:contact@conduitim.pl">
+          contact@conduitim.pl
+        </a>{' '}
+        |{' '}
+        <address className={classNames('p-adr', 'h-adr')}>
+          <span className="p-street-address">380 Hamilton Ave #252</span>,{' '}
+          <span className="p-locality">Palo Alto</span>,{' '}
+          <span className="p-region">CA</span>{' '}
+          <span className="p-postal-code">94301</span>
+        </address>{' '}
+        |{' '}
+        <a className="p-tel" href="tel:1-650-308-4367">
+          +1 (650) 308-4367
+        </a>
+      </footer>
     </div>
   );
 }
